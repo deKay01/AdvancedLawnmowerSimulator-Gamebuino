948b11ffca9e53b1f1fb53bfac8e9814c337e20d
deKay01 <andyk@lofi-gaming.org.uk>
2015-02-27 10:28:31 +0000

    First upload

---
+++ AdvancedLawnmowerSimulator-Gamebuino.ino
@@ -0,0 +1,24 @@
+// ALS
+const char version[] = "0.01";
+// deKay - @deKay01 andyk@lofi-gaming.org.uk
+
+// Very simple, terrible RPG
+
+#include <SPI.h>
+#include <Gamebuino.h>
+
+Gamebuino gb;
+
+extern const byte font3x3[]; //a really tiny font
+extern const byte font3x5[]; //a small but efficient font (default)
+extern const byte font5x7[]; //a large, comfy font
+
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
